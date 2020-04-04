34db8b18d580debae70bda48647dc5b8bc0bda45
icebeam7 <luis.beltran@itcelaya.edu.mx>
2020-04-04 04:30:54 +0200

    Initial version: Android and TensorFlow

---
+++ COIL100_ImageClassifier/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace COIL100_ImageClassifier
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.CustomVisionView();
+        }
+    }
+}
